cc55e1626ac77eb129f6c26842e743efc23f8128
Guido Zuidhof <me@guido.io>
2020-08-06 05:19:10 +0100

    Prep for release

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import * as lithtml from "lit-html";
 (window as any).lithtml = lithtml;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-(window as any).initialNotebookContent = require("./debugNotebooks/promises.nb").default;
+// (window as any).initialNotebookContent = require("./debugNotebooks/promises.nb").default;
 
 document.body.innerHTML += `
 <base target="_parent" />
