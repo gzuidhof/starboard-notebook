af6e3f2e07defd7ed798e10f5567b540416504dc
stefnotch <brandmairstefan@gmail.com>
2021-08-13 16:13:40 +0200

    Change filesystem to use AsyncResult

--- src/filesystem/filesystem.ts
+++ src/filesystem/filesystem.ts
@@ -1,14 +1,23 @@
-export interface NotebookFilesystemError extends Error {
-  status: number;
-}
+export type AsyncResult<T, E = Error> = Promise<
+  | {
+      ok: true;
+      data: T;
+    }
+  | {
+      ok: false;
+      status: number;
+      error: E;
+      detail?: string;
+    }
+>;
 
 /**
  * All of the functions can reject with a `NotebookFilesystemError`
  */
 export interface NotebookFilesystem {
-  get(opts: { path: string }): Promise<string>;
-  put(opts: { path: string; value: string }): Promise<void>;
-  delete(opts: { path: string }): Promise<void>;
-  listFiles(opts: { path: string }): Promise<string[]>;
-  move(opts: { path: string; newPath: string }): Promise<void>;
+  get(opts: { path: string }): AsyncResult<string>;
+  put(opts: { path: string; value: string }): AsyncResult<void>;
+  delete(opts: { path: string }): AsyncResult<void>;
+  listFiles(opts: { path: string }): AsyncResult<string[]>;
+  move(opts: { path: string; newPath: string }): AsyncResult<void>;
 }
